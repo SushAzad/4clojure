d226e7738c91d2a371e9df88a31d1cf575cbad6a
Sushmita Azad <sazad@opploans.com>
2019-06-11 09:32:55 -0500

    Initial commit

---
+++ src/clojure_noob/4clojure.clj
@@ -0,0 +1,88 @@
+(ns clojure-noob.4clojure)
+
+(fn [k m] (and (contains? m k) (nil? (m k))))
+
+;Problem 2
+(= (- 10 (* 2 3)) __)
+;Ans
+(4)
+
+
+;Problem 4
+(= (list __) '(:a :b :c))
+;Ans
+:a :b :c
+
+;Problem  5
+(= __ (conj '(2 3 4) 1))
+
+(= __ (conj '(3 4) 2 1))
+;Ans
+'(1 2 3 4)
+
+;Problem 16: Write a function which returns a personalized greeting
+(= (__ "Dave") "Hello, Dave!")
+
+(= (__ "Jenn") "Hello, Jenn!")
+
+(= (__ "Rhea") "Hello, Rhea!")
+;Ans
+(fn namely [boo] (str "Hello, " boo "!"))
+
+
+;Problem 17: The map function takes two arguments: a function (f) and a sequence (s).
+;Map returns a new sequence consisting of the result of applying f to each item of s.
+;Do not confuse the map function with the map data structure.
+(= __ (map #(+ % 5) '(1 2 3)))
+;Ans:
+'(6 7 8)
+
+;Problem 18: The filter function takes two arguments: a predicate function (f) and a sequence (s).
+; Filter returns a new sequence consisting of all the items of s for which (f item) returns true.
+(= __
+   (filter #(> % 5) '(3 4 5 6 7)))
+;Ans:
+'(6 7)
+
+;Problem 35: Clojure lets you give local names to values using the special let-form.
+
+(= __ (let [x 5] (+ 2 x)))
+(= __ (let [x 3, y 10] (- y x)))
+(= __ (let [x 21] (let [y 3] (/ x y))))
+;Ans:
+7
+
+;Problem 36: Can you bind x, y, and z so that these are all true?
+(= 10 (let __ (+ x y)))
+(= 4 (let __ (+ y z)))
+(= 1 (let __ z))
+;Ans
+[z 1 y 3 x 7]
+
+;Problem 37: Regex patterns are supported with a special reader macro.
+(= __ (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))
+(= __ (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))
+;Ans:
+"ABC"
+
+;Problem 54: A recursive function is a function which calls itself. This is one of the fundamental techniques used in functional programming.
+(= __ ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 5))
+;Ans:
+[5 4 3 2 1]
+
+;Problem 64: Reduce takes a 2 argument function and an optional starting value. It then applies the function to the first 2 items in the sequence (or the starting value and the first element of the sequence). In the next iteration the function will be called on the previous return value and the next item from the sequence, thus reducing the entire collection to one value. Don't worry, it's not as complicated as it sounds.
+(= 15 (reduce __ [1 2 3 4 5]))
+(=  0 (reduce __ []))
+(=  6 (reduce __ 1 [2 3]))
+;Ans:
++
+
+;Problem 71: The -> macro threads an expression x through a variable number of forms. First, x is inserted as the second item in the first form, making a list of it if it is not a list already. Then the first form is inserted as the second item in the second form, making a list of that form if necessary. This process continues for all the forms. Using -> can sometimes make your code more readable.
+(= (__ (sort (rest (reverse [2 5 4 1 3 6]))))
+   (-> [2 5 4 1 3 6] (reverse) (rest) (sort) (__))
+   5)
+;Ans:
+last
+
+
+
---
+++ src/clojure_noob/core.clj
@@ -0,0 +1,92 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main [& _]
+  (println "I'm a little teapot"))
+
+(defn ops []
+      (+ 1 2 3 4))
+(def sush-alias ["yasmin" "minnu" "sloo"])
+
+(ops)
+
+(+ 1 2 3)
+
+(str "It was the pand" "in the library " "with a hammer")
+
+(defn error-mes [severity]
+  (str "OH GOD WE GONNA"
+       (if (= severity :mild) " BE MILDLY INCON" " DED")))
+
+(error-mes :harsh)
+
+(def sushmap {:first "Sush" :last "Azad"})
+
+(defn oohlala [xyz]
+  (get sushmap :last))
+
+(oohlala :abc)
+
+(sushmap :last)
+
+(:bloo sushmap)
+
+
+(def mylist [1 2 3 676])
+(get mylist 8)
+(conj mylist 3 4 7)
+(get mylist 3)
+'(1 2 3 4)
+
+
+" Write a function which takes a function f and a variable number of maps.
+Your function should return a map that consists of the rest of the maps conj-ed onto the first.
+If a key occurs in more than one map, the mapping (s)
+from the latter (left-to-right) should be combined with the mapping in the result by calling (f val-in-result val-in-latter)"
+;(= (__ * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
+;   {:a 4, :b 6, :c 20})
+;
+;(= (__ - {1 10, 2 20} {1 3, 2 10, 3 15})
+;   {1 7, 2 10, 3 15})
+;(= (__ concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
+;   {:a [3 4 5], :b [6 7], :c [8 9]})
+
+(defn doManythings
+  ([f]
+   {})
+
+  ([f m1]
+   m1)
+
+  ([f m1 m2]
+   (let [keys1 (keys m1)
+         keys2 (keys m2)
+         distinct-keys (set (concat keys1 keys2))]
+     (into {}
+           (for [k distinct-keys]
+             [k (cond
+                  (and (contains? m1 k)
+                       (contains? m2 k))
+                  (f (m1 k) (m2 k))
+                  (contains? m1 k) (m1 k)
+                  (contains? m2 k) (m2 k))])))))
+
+(doManythings * {:a  2, :b 3, :c 4} {:a 2})
+
+(defn plz [] (set '(:a :a :b :c :c :c :c :d :d)))
+
+(apply str
+       (re-seq #"[A-Z]+" "bA1B3Ce "))
+
+((fn foo [x] (when (> x 0) (conj (foo (dec x)) x)))5)
+
+(loop [x 5
+       result []]
+  (if (> x 0)
+    (recur (dec x) (conj result (+ 2 x)))
+    result))
+
+(map (partial reduce +) [[1 2 3 4] [5 6 7 8]])
+
+(->> [2 5 4 1 3 6]
+     (drop 2))
